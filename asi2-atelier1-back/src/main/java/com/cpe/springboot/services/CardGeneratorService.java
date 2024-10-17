279d14f1fedf616f3909829adb4d8b8c64c63ee4
LazareC <lazarela17@gmail.com>
2024-10-17 16:55:35 +0200

    hotfix: back work again (or not)

--- asi2-atelier1-back/src/main/java/com/cpe/springboot/services/CardGeneratorService.java
+++ asi2-atelier1-back/src/main/java/com/cpe/springboot/services/CardGeneratorService.java
@@ -2,10 +2,7 @@ package com.cpe.springboot.services;
 
 import com.cpe.springboot.bo.CardModel;
 import com.cpe.springboot.bo.TransactionModel;
-import com.cpe.springboot.common.ImageGenerationRequestDTO;
-import com.cpe.springboot.common.ImageGenerationResponseDTO;
-import com.cpe.springboot.common.TextGenerationRequestDTO;
-import com.cpe.springboot.common.TextGenerationResponseDTO;
+import com.cpe.springboot.common.*;
 import com.cpe.springboot.dto.GenerateCardDTO;
 import com.cpe.springboot.utils.CallbackUrlParser;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +10,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
+import java.net.URL;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,10 +24,16 @@ public class CardGeneratorService {
     private final FileService fileService;
     @Value("${queue.description}")
     private String descriptionQueue;
+    @Value("${queue.image}")
+    private String imageQueue;
+    @Value("${queue.properties}")
+    private String propertiesQueue;
     @Value("${callback.description.url}")
-    private String descriptionCallbackUrl;
+    private URL descriptionCallbackUrl;
     @Value("${callback.image.url}")
-    private String imageCallbackUrl;
+    private URL imageCallbackUrl;
+    @Value("${callback.properties.url}")
+    private URL propertiesCallbackUrl;
 
     public CardGeneratorService(@Lazy TransactionService transactionService, JmsTemplate jmsTemplate, @Lazy CardModelService cardModelService, FileService fileService) {
         this.transactionService = transactionService;
@@ -67,24 +71,40 @@ public class CardGeneratorService {
             cardModel.setImgUrl(filePath);
             cardModelService.updateCard(cardModel);
             transactionService.finishImageGeneration(transactionModel);
+            generateProperties(transactionModel.getUuid(), cardModel);
+
         }
+    }
+
+    private void generateProperties(UUID uuid, CardModel cardModel) {
+        PropGenerationRequestDTO propGenerationRequestDTO = new PropGenerationRequestDTO(uuid, fileService.getUriPath(cardModel.getImgUrl()), CallbackUrlParser.parseCallbackUrl(propertiesCallbackUrl.toString(), uuid.toString()));
+        jmsTemplate.convertAndSend(propertiesQueue, propGenerationRequestDTO);
     }
 
     public void generateDescription(UUID transactionId, String prompt) {
 
-        TextGenerationRequestDTO textGenerationRequestDTO = new TextGenerationRequestDTO(transactionId, prompt, CallbackUrlParser.parseCallbackUrl(descriptionCallbackUrl, transactionId.toString()));
-
+        TextGenerationRequestDTO textGenerationRequestDTO = new TextGenerationRequestDTO(transactionId, prompt, CallbackUrlParser.parseCallbackUrl(descriptionCallbackUrl.toString(), transactionId.toString()));
         jmsTemplate.convertAndSend(descriptionQueue, textGenerationRequestDTO);
     }
 
     public void generateImage(UUID transactionId, String prompt) {
-        ImageGenerationRequestDTO imageGenerationRequestDTO = new ImageGenerationRequestDTO(transactionId, prompt, "", CallbackUrlParser.parseCallbackUrl(imageCallbackUrl, transactionId.toString()));
+        ImageGenerationRequestDTO imageGenerationRequestDTO = new ImageGenerationRequestDTO(transactionId, prompt, "", CallbackUrlParser.parseCallbackUrl(imageCallbackUrl.toString(), transactionId.toString()));
 
-        jmsTemplate.convertAndSend(descriptionQueue, imageGenerationRequestDTO);
+        jmsTemplate.convertAndSend(imageQueue, imageGenerationRequestDTO);
     }
 
 
-    public void finishStatsGeneration() {
-
+    public void finishStatsGeneration(PropGenerationResponseDTO propGenerationResponseDTO) {
+        TransactionModel transactionModel = transactionService.getTransaction(propGenerationResponseDTO.transactionId());
+        Optional<CardModel> card = this.cardModelService.getCard(transactionModel.getCard().getId());
+        if (transactionModel.getStatsGeneratedAt()==null && card.isPresent()) {
+            CardModel cardModel = card.get();
+            cardModel.setHp(propGenerationResponseDTO.properties().getHp());
+            cardModel.setAttack(propGenerationResponseDTO.properties().getAttack());
+            cardModel.setDefence(propGenerationResponseDTO.properties().getDefence());
+            cardModel.setEnergy(propGenerationResponseDTO.properties().getEnergy());
+            cardModelService.updateCard(cardModel);
+            transactionService.finishStatsGeneration(transactionModel);
+        }
     }
 }
