2602a9a03fca17f6f35919d88b8e5b972e616932
gregory <gregory.spinar@gmail.com>
2024-11-20 16:37:19 +0100

    Envie de crever

--- asi2-backend-fight/models/combat.js
+++ asi2-backend-fight/models/combat.js
@@ -32,6 +32,8 @@ class Combat {
         if (!this.participants.has(userId)) throw new Error("Cet utilisateur ne fait pas partie du combat")
         if (this.userCards[userId].keys().size === this.#maxCardsPerFighter) throw new Error("Nombre maximal de cartes atteinte !")
 
+        if (!userCards) throw new Error("Cet utilisateur ne fait pas partie du combat")
+        if (userCards.size === this.#maxCardsPerFighter) throw new Error("Nombre maximal de cartes atteinte !")
 
         this.userCards[userId][card.id] = card;
         this.isCombatReady = this.#allUserSelectedCards();
